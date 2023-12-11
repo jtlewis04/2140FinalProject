ff17e9d5cb73885ece00dbc3ed87d7c61c815a56
jtlewis04 <screeper101@gmail.com>
2023-12-10 23:22:16 -0500

    Added docstrings and more comments

--- HumanVsAi.py
+++ HumanVsAi.py
@@ -50,6 +50,13 @@ class Missile(arcade.Sprite):
 
 class HumanVsAiView(arcade.View):
     def __init__(self):
+        """
+        Class constructor, initializes class attributes that are used throughout the class, calls setup
+        Parameters:
+            - self (HumanVsAI): This class instance
+        Returns:
+            None
+        """
         super().__init__()
         arcade.set_background_color(arcade.color.BLACK)
 
@@ -80,6 +87,15 @@ class HumanVsAiView(arcade.View):
         self.setup()
 
     def setup(self):
+        """
+        Game setup, includes loading background and initializing player, AI player, score, and empty lists
+        for enemies, missiles, and explosions then adds them all to scene
+        Parameters:
+            - self (HumanVsAI): This class instance
+        
+        Returns:
+            None
+        """
         # Get file path to TileMap
         map_path = ASSETS_PATH + '/galaga_map.tmx'
 
@@ -114,6 +130,15 @@ class HumanVsAiView(arcade.View):
 
         
     def on_draw(self):
+        """
+        This function renders the state of the game regardless of what it is, leaves state unchanged
+
+        Parameters:
+            - self (HumanVsAI): This class instance
+        
+        Returns:
+            None
+        """
         arcade.start_render()
         # Clear the screen
         self.clear()
@@ -136,6 +161,17 @@ class HumanVsAiView(arcade.View):
             arcade.draw_text(f"Lvl {self.wave//3+1} AI: Sharpshooter", 620, SCREEN_HEIGHT - 25, arcade.color.WHITE, 20) 
 
     def on_update(self, delta_time):
+        """
+        This function updates the state of the game every tick, includes AI behavior, hit-box collision detection,
+        enemy spawning, and checking if game is over
+
+        Parameters:
+            - self (HumanVsAI): This class instance
+            - delta_time (float): Time since this function last ran
+        
+        Returns:
+            None
+        """
         if self.wave == 13:
             self.game_over = 2
         # Check for game_over state
@@ -280,6 +316,7 @@ class HumanVsAiView(arcade.View):
                     # Make an explosion on player who died
                     # Make an explosion on enemy
                     for i in range(ex.PARTICLE_COUNT):
+                        # Adds a particle to the explosion, number of particles determined by preset value
                         explosions_list = self.scene.get_sprite_list('explosions')
                         particle_e = ex.Particle(explosions_list)
                         particle_e.position = enemy.position
@@ -287,26 +324,41 @@ class HumanVsAiView(arcade.View):
                         particle_p.position = player.position
                         explosions_list.append(particle_e)
                         explosions_list.append(particle_p)
+                    #Smoke is added with a size of 50 pixels at explosion site
                     smoke_e = ex.Smoke(50)
                     smoke_p = ex.Smoke(50)
                     smoke_e.position = enemy.position
                     smoke_p.position = player.position
                     explosions_list.append(smoke_e)
                     explosions_list.append(smoke_p)
+                    # Enemy and Player are removed from SpriteList (no longer displayed)
                     enemy.kill()
                     player.kill()
 
             # Missiles and enemies update regardless of player movement
             self.scene.update(['enemies','missiles','explosions'])
         else:
+            # If game has been over for 2 seconds, display GameOver screen, window must be resized for new screen
             game_over_view = GameOverView(self.score,self.wave, self.game_over)
             self.window.set_size(480,710)
             self.window.show_view(game_over_view)
 
     def on_key_press(self, key, modifiers):
+        """
+        Sets player movement to left and right arrow keys, Space to shoot missiles, and Escape to pause and quit to menu
+        Parameters:
+            - self (HumanVsAI): This class instance
+            - key (arcade.key): key being released
+            - modifiers: extra state of key being released (Ex. Caps Lock or holding Shift/Ctrl)
+        
+        Returns:
+            None
+        """
         if key == arcade.key.LEFT:
+            # Movement to the left is in the -x direction
             self.player.change_x = -5
         elif key == arcade.key.RIGHT:
+            # Movement to the right is in the +x direction
             self.player.change_x = 5
         elif key == arcade.key.SPACE:
             # Ensure a 0.5 second delay between missile firing
@@ -321,12 +373,29 @@ class HumanVsAiView(arcade.View):
             
 
     def on_key_release(self, key, modifiers):
+        """
+        Sets player movement to 0 when left or right arrow key is released
+        Parameters:
+            - self (HumanVsAI): This class instance
+            - key (arcade.key): key being released
+            - modifiers: extra state of key being released (Ex. Caps Lock or holding Shift/Ctrl)
+        
+        Returns:
+            None
+        """
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.player.change_x = 0
 
 # Title screen
 class TitleView(arcade.View):
     def __init__(self, paused_game = None) -> None:
+        """
+        Class constructor, initializes class attributes that are used throughout the class
+        Parameters:
+            - self (TitleView): This class instance
+        Returns:
+            None
+        """
         super().__init__()
         #Load title image
         self.title_image = arcade.load_texture(ASSETS_PATH+'/title_screen.png')
@@ -345,6 +414,16 @@ class TitleView(arcade.View):
     
     
     def on_update(self, delta_time: float) -> None:
+        """
+        This function updates the state of the menu every tick, used for blinking text on screen
+
+        Parameters:
+            - self (TitleView): This class instance
+            - delta_time (float): Time since this function last ran
+        
+        Returns:
+            None
+        """
         # First, count down the time
         self.display_timer -= delta_time
 
@@ -359,6 +438,16 @@ class TitleView(arcade.View):
 
             
     def on_draw(self) -> None:
+        """
+        This function renders the state of the menu, includes background image, title text, and instructional text
+        text shown depends on state of boolean variables determined in on_update and on_key_press
+
+        Parameters:
+            - self (TitleView): This class instance
+        
+        Returns:
+            None
+        """
         # Start the rendering loop
         arcade.start_render()
 
@@ -433,6 +522,18 @@ class TitleView(arcade.View):
 
         
     def on_key_press(self, key: int, modifiers: int) -> None:
+        """
+        Detects Space to start or resume game depending on boolean variable, R to restart if paused, I to show instructions
+        Parameters:
+            - self (TitleView): This class instance
+            - key (arcade.key): key being released
+            - modifiers: extra state of key being released (Ex. Caps Lock or holding Shift/Ctrl)
+        
+        Returns:
+            None
+        """
+        # If menu isn't shown during a game, only show detect Space (start) and I (instructions)
+        # If game is paused, detect Space (resume), I (instructions), and R (restart)
         if self.paused_game == None:
             if key == arcade.key.SPACE:
                 if not self.showing_instructions:
@@ -461,6 +562,13 @@ class TitleView(arcade.View):
 
 class GameOverView(arcade.View):
     def __init__(self, score, wave, game_over) -> None:
+        """
+        Class constructor, initializes class attributes that are used throughout the class
+        Parameters:
+            - self (TitleView): This class instance
+        Returns:
+            None
+        """
         super().__init__()
         #Load title image
         self.title_image = arcade.load_texture(ASSETS_PATH+'/title_screen.png')
@@ -476,6 +584,16 @@ class GameOverView(arcade.View):
         self.wave = wave
 
     def on_update(self, delta_time: float) -> None:
+        """
+        This function updates the state of the GameOver menu every tick, used for blinking text on screen
+
+        Parameters:
+            - self (GameOverView): This class instance
+            - delta_time (float): Time since this function last ran
+        
+        Returns:
+            None
+        """
         # First, count down the time
         self.display_timer -= delta_time
 
@@ -486,6 +604,16 @@ class GameOverView(arcade.View):
             self.display_timer = 2
 
     def on_draw(self) -> None:
+        """
+        This function renders the state of the GameOver screen including defeat or victory text, final score, wave reached, 
+        and level of AI reached before death. Shows a flashing option to return to menu
+
+        Parameters:
+            - self (GameOverView): This class instance
+        
+        Returns:
+            None
+        """
         # Start the rendering loop
         arcade.start_render()
 
@@ -548,16 +676,27 @@ class GameOverView(arcade.View):
             )
         
     def on_key_press(self, key: int, modifiers: int) -> None:
+        """
+        Detects Escape to return to TitleView
+            - self (GameOverView): This class instance
+            - key (arcade.key): key being released
+            - modifiers: extra state of key being released (Ex. Caps Lock or holding Shift/Ctrl)
+        
+        Returns:
+            None
+        """
         if key == arcade.key.ESCAPE:
             title_view = TitleView()
             self.window.show_view(title_view)
             
 
 def main():
+    # Create menu window
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, "HumanVsAi")
+    # Set window to show a TitleView
     title_view = TitleView()
-    window.set_size(480,710)
     window.show_view(title_view)
+    # Start running the active View (starts updating state every tick)
     arcade.run()
 
 if __name__ == "__main__":
